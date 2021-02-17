e5b1063abcb8ae0bd5f820a7b8c1c05a59f285a0
Binaya Baral <binaya.baral@p2h.com>
2021-02-17 14:35:00 +0545

    broker details page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import DashboardScreen from './screens/DashboardScreen';
 import CurrencyExchangeScreen from './screens/CurrencyExchangeScreen';
 import AllStocksScreen from './screens/AllStocksScreen';
+import AllBrokersScreen from './screens/AllBrokersScreen';
 
 function App() {
 	return (
@@ -14,6 +15,7 @@ function App() {
 					<Route path="/dashboard" component={DashboardScreen}></Route>
 					<Route path="/exchange-rates" component={CurrencyExchangeScreen}></Route>
 					<Route path="/all-stocks" component={AllStocksScreen}></Route>
+					<Route path="/all-brokers" component={AllBrokersScreen}></Route>
 				</Switch>
 			</main>
 		</Router>
